aeb9cc5dc52ebede3e37ee6fddac623bcc6d5bc4
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-15 20:24:49 +0530

    ✨ Non Blocking approach

--- NodeJS/FileStreams/app.js
+++ NodeJS/FileStreams/app.js
@@ -2,16 +2,16 @@ const fs = require("node:fs");
 
 console.log("Started");
 // Non Blocking
-// fs.readFile("Input.txt", (err, dataFromFile) => {
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     console.log(dataFromFile.toString());
-//   }
-// });
+fs.readFile("Input.txt", (err, dataFromFile) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(dataFromFile.toString());
+  }
+});
 
 // Blocking
-const dataFromFile = fs.readFileSync("Input.txt");
-console.log(dataFromFile.toString());
+// const dataFromFile = fs.readFileSync("Input.txt");
+// console.log(dataFromFile.toString());
 
 console.log("Ended");
