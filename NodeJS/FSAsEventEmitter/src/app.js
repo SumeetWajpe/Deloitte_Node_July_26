aa1dc9686b1c0efab6982cf18fe8243c99c108d6
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-16 19:11:22 +0530

    ✨ Using WriteStream

--- NodeJS/FSAsEventEmitter/src/app.js
+++ NodeJS/FSAsEventEmitter/src/app.js
@@ -1,12 +1,16 @@
 import fs from "fs";
 
 const readStream = fs.createReadStream("src/Input.txt");
+const writeStream = fs.createWriteStream("src/Output.txt");
+let dataToBeWritten = "";
 readStream.on("data", chunk => {
   console.log(
     ">>>>>>>>>>>>>>>>>>>>>>>>>>> CHUNK received >>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
   );
+  dataToBeWritten += chunk;
 });
 
 readStream.on("end", () => {
   console.log("Reading of file done.. ");
+  writeStream.write(dataToBeWritten);
 });
