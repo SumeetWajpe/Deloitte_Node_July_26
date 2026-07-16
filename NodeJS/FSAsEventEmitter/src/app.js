72b810e2895bb3e4337b33fe2fefdc655251071a
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-16 19:18:12 +0530

    ✨ Using pipe()

--- NodeJS/FSAsEventEmitter/src/app.js
+++ NodeJS/FSAsEventEmitter/src/app.js
@@ -2,15 +2,18 @@ import fs from "fs";
 
 const readStream = fs.createReadStream("src/Input.txt");
 const writeStream = fs.createWriteStream("src/Output.txt");
-let dataToBeWritten = "";
-readStream.on("data", chunk => {
-  console.log(
-    ">>>>>>>>>>>>>>>>>>>>>>>>>>> CHUNK received >>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
-  );
-  dataToBeWritten += chunk;
-});
+// let dataToBeWritten = "";
+// readStream.on("data", chunk => {
+//   console.log(
+//     ">>>>>>>>>>>>>>>>>>>>>>>>>>> CHUNK received >>>>>>>>>>>>>>>>>>>>>>>>>>>>>",
+//   );
+//   dataToBeWritten += chunk;
+// });
 
-readStream.on("end", () => {
-  console.log("Reading of file done.. ");
-  writeStream.write(dataToBeWritten);
-});
+// readStream.on("end", () => {
+//   console.log("Reading of file done.. ");
+//   writeStream.write(dataToBeWritten);
+// });
+
+// OR
+readStream.pipe(writeStream);
