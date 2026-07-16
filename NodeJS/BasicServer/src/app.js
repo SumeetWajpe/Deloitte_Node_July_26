8a9d7d70328638c73341e1cff446fdc5fd8566bc
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-16 20:21:15 +0530

    ✨ Woring with urls | sending styles.css as response

--- NodeJS/BasicServer/src/app.js
+++ NodeJS/BasicServer/src/app.js
@@ -3,14 +3,27 @@ import { createServer } from "node:http";
 import fs from "node:fs";
 
 const server = createServer((req, res) => {
-  fs.readFile("src/Index.html", (err, data) => {
-    if (err) {
-      console.log(err.message);
-    } else {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    }
-  });
+  console.log(req.url);
+  if (req.url == "/") {
+    fs.readFile("src/Index.html", (err, data) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
+  if (req.url == "/styles.css") {
+    fs.readFile("src/styles.css", (err, data) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        res.writeHead(200, { "Content-Type": "text/css" });
+        res.end(data);
+      }
+    });
+  }
 });
 
 // starts a simple http server locally on port 3000
