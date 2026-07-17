55dc64903ce4b4829467946f194b10b1c4e681c2
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-17 20:15:16 +0530

    ✨ Using express.static middleware

--- NodeJS/ExpressBasics/app.js
+++ NodeJS/ExpressBasics/app.js
@@ -2,10 +2,30 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// middlewares
+app.use(express.static("src/static"));
+
 // register an endpoint for GEt verb with path as /
 app.get("/", (req, res) => {
   // res.send("<h1>Hello World!</h1>");
   res.sendFile("Index.html", { root: __dirname });
+});
+
+// app.get("/script.js", (req, res) => {
+//   // res.send("<h1>Hello World!</h1>");
+//   res.sendFile("Index.html", { root: __dirname });
+// });
+// app.get("/styles.css", (req, res) => {
+//   // res.send("<h1>Hello World!</h1>");
+//   res.sendFile("Index.html", { root: __dirname });
+// });
+
+app.get("/products", (req, res) => {
+  var products = [
+    { id: 1, title: "MacBookPro" },
+    { id: 2, title: "LED TV" },
+  ];
+  res.json(products);
 });
 
 app.listen(port, () => {
---
+++ NodeJS/ExpressBasics/src/static/script.js
@@ -0,0 +1,1 @@
+console.log("Hello from Node executed in browser !");
