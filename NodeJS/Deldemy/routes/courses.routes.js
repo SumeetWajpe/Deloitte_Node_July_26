da05ae004d30696cc70f5e4fb1084230b5c4dd45
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-07-27 18:11:52 +0530

    ✨ Comments added for MySQL

--- NodeJS/Deldemy/routes/courses.routes.js
+++ NodeJS/Deldemy/routes/courses.routes.js
@@ -5,14 +5,21 @@ const path = require("path");
 const fs = require("fs");
 
 router.get("/courses", (req, res) => {
+  // Get data from DB
   res.json(courses);
 });
 
 router.get("/coursedetails/:id", (req, res) => {
+  // Get data from DB
   let courseId = req.params.id;
   let theCourse = courses.find(c => c.id == courseId);
   // html
   res.render("coursedetails", { title: "Course Details", theCourse });
+});
+
+router.post("/newcourse", (req, res) => {
+  // read the json from req.body
+  // insert data in DB
 });
 
 router.get("/video", (req, res) => {
