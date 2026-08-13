29d43b0bb9291603572234ee9b511bae99704edb
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2026-08-13 17:29:12 +0530

    ✨ Using Microservices with Node (Training app - Deldemy)

---
+++ NodeJS/online-training-microservices/query/app.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const CourseWithReviews = require("./models/courseswithreviews.model");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.LOCAL_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("open", () => {
+  console.log("Connected to query DB  !");
+});
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+//get all courses
+app.get("/courses", async (req, res) => {
+  let listofcourses = await CourseWithReviews.find({});
+  res.json(listofcourses);
+});
+
+app.post("/events", async (req, res) => {
+  // logic for inserting new course/new review in querydb
+  let { type, payload } = req.body;
+  console.log("Within Query Service !");
+
+  if (type == "CourseCreated") {
+    let courseToBeAdded = new CourseWithReviews({ ...payload, reviews: [] });
+    await courseToBeAdded.save();
+  }
+
+  res.json({ status: "OK" });
+});
+
+module.exports = app;
